8ad7344e3543519d5203df0f3d5a8d686317ae99
GuanZhichao <754809934@qq.com>
2022-10-16 22:27:21 +0800

    项目初始化-antd组件库的使用

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react'
-
+import { Button, DatePicker } from 'antd'
 export default class Login extends Component {
   render() {
-    return <div>登录组件</div>
+    return (
+      <div>
+        登录组件 <Button type="primary">我是一个按钮</Button> <DatePicker />
+      </div>
+    )
   }
 }
