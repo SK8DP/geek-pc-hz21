450f614023575f2a77c5cf8dcba02358216ba38d
GuanZhichao <754809934@qq.com>
2022-10-17 11:43:41 +0800

    登录功能-基本结构

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
-import { Button, DatePicker } from 'antd'
+import { Card } from 'antd'
+import './index.css'
+import logo from '../../assets/logo.png'
 export default class Login extends Component {
   render() {
     return (
-      <div>
-        登录组件 <Button type="primary">我是一个按钮</Button> <DatePicker />
+      <div className='login'>
+        <Card className="login-container">
+          <img src={logo} alt="" className='login-logo' /> {/*react里的图片必须显示导入*/}
+        </Card>
       </div>
     )
   }
